097f56bf10e7e70a6f3cf50b11a305cfe7fd7d3a
KakumaniRuthwik <ruthwikkakumani08@gmail.com>
2025-08-27 19:05:56 +0530

    feat: implement Majority Element II solution using Boyer-Moore algorithm

--- Data Structures and Algorithms/Advanced/Arrays/Hard/Majority Element II/majority_element_II_optimal.cpp
+++ Data Structures and Algorithms/Advanced/Arrays/Hard/Majority Element II/majority_element_II_optimal.cpp
@@ -1,46 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> majorityElement(vector<int>& nums) {
-    int n = nums.size();
-    int cand1 = 0, cand2 = 0;
-    int cnt1 = 0, cnt2 = 0;
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int element1 = 0, element2 = 0;
+        int count1 = 0, count2 = 0;
 
-    // First pass: Find potential candidates
-    for (int num : nums) {
-        if (num == cand1) {
-            cnt1++;
-        } else if (num == cand2) {
-            cnt2++;
-        } else if (cnt1 == 0) {
-            cand1 = num;
-            cnt1 = 1;
-        } else if (cnt2 == 0) {
-            cand2 = num;
-            cnt2 = 1;
-        } else {
-            cnt1--;
-            cnt2--;
+        for (int num : nums) {
+            if (num == element1) {
+                count1++;
+            } else if (num == element2) {
+                count2++;
+            } else if (count1 == 0) {
+                element1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                element2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
         }
+
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == element1) count1++;
+            else if (num == element2) count2++;
+        }
+
+        vector<int> result;
+        int threshold = n / 3;
+        if (count1 > threshold) result.push_back(element1);
+        if (count2 > threshold) result.push_back(element2);
+
+        sort(result.begin(), result.end()); // ascending order
+        return result;
     }
-
-    // Second pass: Validate candidates
-    cnt1 = cnt2 = 0;
-    for (int num : nums) {
-        if (num == cand1) cnt1++;
-        else if (num == cand2) cnt2++;
-    }
-
-    vector<int> result;
-    if (cnt1 > n / 3) result.push_back(cand1);
-    if (cnt2 > n / 3) result.push_back(cand2);
-
-    return result;
-}
+};
 
 int main() {
-    vector<int> nums = {3,2,3};
-    vector<int> ans = majorityElement(nums);
+    vector<int> nums = {1, 2, 1, 1, 3, 2, 2, 3};
+    Solution s;
+    vector<int> ans = s.majorityElement(nums);
     for (int x : ans) cout << x << " ";
     return 0;
 }
