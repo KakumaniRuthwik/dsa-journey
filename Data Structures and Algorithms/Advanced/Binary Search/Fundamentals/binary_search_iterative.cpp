05c50515c44cd8318e378a89ffda2273a0a22561
KakumaniRuthwik <ruthwikkakumani08@gmail.com>
2025-09-06 22:51:30 +0530

    feat: Implement iterative binary search.

---
+++ Data Structures and Algorithms/Advanced/Binary Search/Fundamentals/binary_search_iterative.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int target) {
+    int low = 0, high = nums.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 5, 7, 9, 11};
+    int target = 7;
+
+    int result = binarySearch(nums, target);
+    cout << "Target found at index: " << result << endl;
+}
