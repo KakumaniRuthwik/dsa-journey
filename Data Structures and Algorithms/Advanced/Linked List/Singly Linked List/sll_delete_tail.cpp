5f11486f32a96d2d416d22c3f67cf89dd4fc2d92
KakumaniRuthwik <ruthwikkakumani08@gmail.com>
2025-11-24 12:26:29 +0530

    feat: implement deletion at kth position of singly linked list

---
+++ Data Structures and Algorithms/Advanced/Linked List/Singly Linked List/sll_delete_tail.cpp
@@ -0,0 +1,202 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int value){
+        data = value;
+        next = nullptr;
+    }
+};
+
+pair<Node*,Node*> arrayToLinkedList(vector<int>& arr){
+    
+    int size = arr.size();
+
+    if(size == 0){
+        return {nullptr, nullptr};
+    }
+
+    Node* head = new Node(arr[0]);
+    Node* temp = head;
+
+    Node* tail = temp;
+    
+    for(int i = 1; i < size; i++){
+        temp->next = new Node(arr[i]);
+        temp = temp->next;
+        tail = temp;
+    }
+
+    return {head, tail};
+}
+
+void linkedListTraversal(Node* head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << " -> ";
+        temp = temp -> next;
+    }
+
+    cout << "NULL";
+}
+
+void insertAtBeginning(Node* &head,int data){
+    Node* temp = new Node(data);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtEnd(Node* &tail, int data){
+    Node* temp = new Node(data);
+    tail -> next = temp;
+    tail = temp;
+}
+
+void insertAtPosition(Node* &head, Node* &tail, int position, int data){
+    if(position == 0){
+        insertAtBeginning(head, data);
+        return;
+    }
+
+    Node* temp = head;
+    int index = 0;
+
+    while(temp != nullptr && index < position - 1){
+        temp = temp->next;
+        index++;
+    }
+
+    if(temp == nullptr){
+        cout << "Position out of bounds";
+        return;
+    }
+    
+    if(temp->next == nullptr){
+        insertAtEnd(tail, data);
+        return;
+    }
+
+    Node* newNode = new Node(data);
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void deletionAtBeginning(Node* &head){
+
+    if(head == nullptr){
+        return;
+    }
+
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+}
+
+void deleteAtEnd(Node* &head){
+    if(head == nullptr){
+        return;
+    }
+
+    if(head->next == nullptr){
+        delete head;
+        head = nullptr;
+        return;
+    }
+
+    Node* temp = head;
+    while(temp->next->next != nullptr){
+        temp = temp->next;
+    }
+    
+    delete temp->next;
+    temp->next = nullptr;
+}
+
+void deleteAtPosition(Node* &head, int position){
+    if(head == nullptr){
+        return;
+    }
+
+    if(position == 0){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+
+    Node* temp = head;
+    int index = 1;
+    while(temp != nullptr && index < position - 1){
+        temp = temp->next;
+        index++;
+    }
+
+    if(temp == nullptr || temp->next == nullptr){
+        return;
+    }
+
+    Node* toDelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete toDelete;
+
+}
+
+int main(){
+
+    vector<int> arr = {1,2,3,4,6,7,8,9};
+
+    auto [head, tail] = arrayToLinkedList(arr);
+    
+    cout << endl;
+    
+    cout << "Linked List before adding Elements at beginning : " ;
+    linkedListTraversal(head);
+    
+    cout << endl;
+    
+    insertAtBeginning(head, 0);
+    cout << "Linked List after adding Elements at beginning : " ;
+    linkedListTraversal(head);
+
+    cout << endl;
+
+    insertAtEnd(tail, 10);
+    cout << "Linked List after adding Elements at end : " ;
+    linkedListTraversal(head);
+
+    cout << endl;
+
+    insertAtPosition(head, tail, 5, 5);
+    cout << "Linked List after adding Elements at position 5 : " ;
+    linkedListTraversal(head);
+
+    cout << endl;
+
+    deletionAtBeginning(head);
+    cout << "Linked List after deleting element at start : ";
+    linkedListTraversal(head);
+    
+    cout << endl;
+
+    deleteAtEnd(head);
+    cout << "Linked list after deleting element at end : ";
+    linkedListTraversal(head);
+
+    cout << endl;
+
+    deleteAtPosition(head, 5);
+    cout << "Linked list after deleting element at 5th position : ";
+    linkedListTraversal(head);
+
+    cout << endl;
+
+    
+    return 0;
+}
